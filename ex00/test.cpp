974a27558fb96a805e7d570c2c2d450549ac95cf
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2025-07-22 23:43:18 +0000

    string in c++

--- ex00/test.cpp
+++ ex00/test.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
 
 int	main()
 {
-	std::cout << 3 + 3 << std::endl;
+	std::string name = "Houda"; // smthg new in c++, u should just include the string header file :D
+	int			age = 18;
+	char		school[] = "1337";
+
+	// std::cout << 3 + 3 << std::endl;
+	std::cout << "\nHello my name is " << name << " I'm " << age << " and i'm studying IT at " << school << std::endl << std::endl;
+
 	return (0);
 }
