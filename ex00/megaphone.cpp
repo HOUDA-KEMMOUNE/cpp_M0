8e55f3de9e9c32dd4480425f7c45467d44f598f7
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2025-07-24 09:11:20 +0000

    Issue in check_space

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -20,52 +20,125 @@ int	is_space(char c)
 		return (0); //isn't space
 }
 
-char	*skip_spaces(char *str)
+int	end_of_word(char *ret)
 {
-	int		i;
-	int		start;
-	char	*ret;
+	int	i;
 
 	i = 0;
-	while (str[i])
+	while (ret)
 	{
-		while (is_space(str[i]) == 1 || is_space(str[i]) == -1)
+		if ((is_space(ret[i]) == 0) && (is_space(ret[i + 1]) == 1 || is_space(ret[i + 1]) == -1)) 
 		{
-			if (is_space(str[i]) == -1)
-				break ;
-			i++;
-		}
-		start = i;
-		std::cout << "start => " << start << std::endl;
-		if ((is_space(str[i]) == 0) && (is_space(str[i + 1]) == 1 || is_space(str[i + 1]) == -1)) 
-		{
-			if (is_space(str[i]) == 0 && is_space(str[i + 1]) == -1)
+			if (is_space(ret[i]) == 0 && is_space(ret[i + 1]) == -1)
 				break ;
 			else
 			{
 				i++;
-				while ((is_space(str[i]) == 1 || is_space(str[i]) == -1))
+				while ((is_space(ret[i]) == 1 || is_space(ret[i]) == -1))
 				{
-					if (is_space(str[i]) == -1)
+					if (is_space(ret[i]) == -1)
 						break ;
 					else
+					{
 						i++;
+					}
 				}
 			}
 		}
 		i++;
 	}
-	// ret = str[5];
+}
+
+char	*skip_spaces(char *str)
+{
+	int		i;
+	// int		start;
+	char	*ret;
+
+	i = 0;
+	while (is_space(*str) == 1 || is_space(*str) == -1)
+	{
+		if (is_space(*str) == -1)
+			break ;
+		i++;
+		str++;
+	}
+	i = 0;
+	ret = str;
+	// while (ret)
+	// {
+	// 	if ((is_space(ret[i]) == 0) && (is_space(ret[i + 1]) == 1 || is_space(ret[i + 1]) == -1)) 
+	// 	{
+	// 		if (is_space(ret[i]) == 0 && is_space(ret[i + 1]) == -1)
+	// 			break ;
+	// 		else
+	// 		{
+	// 			i++;
+	// 			while ((is_space(ret[i]) == 1 || is_space(ret[i]) == -1))
+	// 			{
+	// 				if (is_space(ret[i]) == -1)
+	// 					break ;
+	// 				else
+	// 				{
+	// 					i++;
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	i++;
+	// }
 	std::cout << "ret ==" << ret << "|" << std::endl;
 
 	return (ret);
 }
+
+// char	*skip_spaces(char *str)
+// {
+// 	int		i;
+// 	int		start;
+// 	char	*ret;
+
+// 	i = 0;
+// 	while (str[i])
+// 	{
+// 		while (is_space(str[i]) == 1 || is_space(str[i]) == -1)
+// 		{
+// 			if (is_space(str[i]) == -1)
+// 				break ;
+// 			i++;
+// 		}
+// 		start = i;
+// 		std::cout << "start => " << start << std::endl;
+// 		if ((is_space(str[i]) == 0) && (is_space(str[i + 1]) == 1 || is_space(str[i + 1]) == -1)) 
+// 		{
+// 			if (is_space(str[i]) == 0 && is_space(str[i + 1]) == -1)
+// 				break ;
+// 			else
+// 			{
+// 				i++;
+// 				while ((is_space(str[i]) == 1 || is_space(str[i]) == -1))
+// 				{
+// 					if (is_space(str[i]) == -1)
+// 						break ;
+// 					else
+// 						i++;
+// 				}
+// 			}
+// 		}
+// 		i++;
+// 	}
+// 	// ret = str[5];
+// 	std::cout << "ret ==" << ret << "|" << std::endl;
+
+// 	return (ret);
+// }
 
 void	str_upper(char *str)
 {
 	int	i;
 
 	i = 0;
+	str = skip_spaces(str);
 	while (str[i])
 	{
 		if (check_str(str[i]) == 1)
@@ -81,7 +154,7 @@ int	main(int ac, char **av)
 {
 	// (void)av;
 	int		i;
-	char	*str;
+	// char	*str;
 	// int	j;
 
 	if (ac == 1)
@@ -91,9 +164,9 @@ int	main(int ac, char **av)
 		i = 1;
 		while (av[i])
 		{
-			str = skip_spaces(av[i]);
-			str_upper(str); //TODO
-			std::cout << str;
+			// skip_spaces(av[i]);
+			str_upper(av[i]); //TODO
+			std::cout << av[i];
 			if (av[i + 1] != NULL)
 				std::cout << " ";
 			i++;
